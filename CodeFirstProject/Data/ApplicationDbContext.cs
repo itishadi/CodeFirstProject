483aa68d337d4c60f01da7176fb7d2233738f58d
Hadi Rasouli <hadirasouli786@hotmail.com>
2023-04-11 10:25:51 +0200

    Done with setup

---
+++ CodeFirstProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- CodeFirstProject/Program.cs
+++ CodeFirstProject/Program.cs
@@ -1,9 +1,34 @@
+using CodeFirstProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+
+
+// Lägg till min DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Lägg till min DataInitializer med Dependency Injection
+builder.Services.AddTransient<DataInitializer>();
+
+
+
 var app = builder.Build();
+
+
+
+
+// Kör min SeedData() metod
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetService<DataInitializer>().MigrateData();
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
